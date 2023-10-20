c620bd03b8ae13c1fa8e8965e4c02c98626edd47
Nino <84574414+elNino0916@users.noreply.github.com>
2023-10-20 23:58:39 +0200

    Deleted old class

--- reShutLegacy/variables.cs
+++ reShutLegacy/variables.cs
@@ -9,7 +9,7 @@ namespace reShutLegacy
     internal class variables
     {
         public static bool prerelease = false;
-        public static string buildid = "3199f6f";
+        public static string buildid = "b9b909c";
         public static string version = "v.11.2.5 (" + buildid + ")";
     }
 }
