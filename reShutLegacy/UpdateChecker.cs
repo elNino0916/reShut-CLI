0a05c877ed0c1e7e0ac64f31ff3192b5724e265a
Nino <84574414+elNino0916@users.noreply.github.com>
2023-12-16 12:00:56 +0100

    1.0.0

---
+++ reShutLegacy/UpdateChecker.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace reShutLegacy
+{
+    internal class UpdateChecker
+    {
+        private static bool updateCheckPerformed = false;
+        private static string updateResultMessage = string.Empty;
+        private static string updateResultMessageLine2 = string.Empty;
+
+        public static void DisplayCenteredMessage(string message)
+        {
+            var consoleWidth = Console.WindowWidth;
+            var padding = (consoleWidth - message.Length) / 2;
+
+            Console.WriteLine(new string(' ', padding) + message);
+        }
+
+        public static async Task MainCheck()
+        {
+            // Check if update has already been checked
+            if (updateCheckPerformed)
+            {
+                // Output the stored result
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                DisplayCenteredMessage(updateResultMessage);
+                DisplayCenteredMessage(updateResultMessageLine2);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                return;
+            }
+
+            const string repositoryUrl = "https://api.github.com/repos/elnino0916/reshut-legacy/releases/latest";
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("reShutLegacy_UpdateSearch");
+
+            var response = await client.GetAsync(repositoryUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var release = JsonSerializer.Deserialize<GitHubRelease>(json);
+
+                var latestVersion = release.tag_name;
+                var currentVersion = Variables.version;
+
+                if (IsNewerVersionAvailable(currentVersion, latestVersion))
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    updateResultMessage = $"A new version ({latestVersion}) of reShut-Legacy is available! Please download the update from";
+                    DisplayCenteredMessage(updateResultMessage);
+                    updateResultMessageLine2 = "https://github.com/elnino0916/reShut-Legacy/releases/latest";
+                    DisplayCenteredMessage(updateResultMessageLine2);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    updateResultMessage = Variables.Motd();
+                    DisplayCenteredMessage(updateResultMessage);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                updateResultMessage = $"Failed to check for updates: {response.StatusCode}. Restart reShut-Legacy to refresh.";
+                DisplayCenteredMessage(updateResultMessage);
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+
+            // Set the flags to indicate that update check has been performed
+            updateCheckPerformed = true;
+        }
+
+        static bool IsNewerVersionAvailable(string currentVersion, string latestVersion)
+        {
+            if (string.IsNullOrEmpty(currentVersion) || string.IsNullOrEmpty(latestVersion))
+                return false;
+
+            return new Version(latestVersion) > new Version(currentVersion);
+        }
+
+        public class GitHubRelease
+        {
+            #pragma warning disable IDE1006
+            public string tag_name { get; set; }
+            #pragma warning restore IDE1006
+        }
+    }
+}
---
+++ reShutLegacy/cmdLine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace reShutLegacy
+{
+    internal class CmdLine
+    {
+        public CmdLine(IEnumerable<string> args)
+        {
+            List<string> validPrefixes = ["-", "/"];
+
+            foreach (var arg in args)
+            {
+                var prefix = validPrefixes.FirstOrDefault(p => arg.StartsWith(p));
+                if (prefix != null)
+                {
+                    // Remove the prefix to get the option name
+                    var option = arg[prefix.Length..].ToLower();
+
+                    switch (option)
+                    {
+                        case "r":
+                            Handler.Reboot();
+                            break;
+                        case "l":
+                            Handler.Logoff();
+                            break;
+                        case "s":
+                            Handler.Shutdown(); 
+                            break;
+                        default:
+                            Console.WriteLine($"Unknown argument: {arg}");
+                            break;
+                    }
+                }
+                else
+                {
+                    // Handle arguments without a valid prefix
+                    Console.WriteLine($"Unknown argument: {arg}");
+                }
+            }
+        }
+    }
+}
