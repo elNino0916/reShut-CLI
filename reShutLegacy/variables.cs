e089cb55086f832730124f0cc60b7548998ce94e
Nino <84574414+elNino0916@users.noreply.github.com>
2024-06-29 13:17:55 +0200

    RC

--- reShutLegacy/variables.cs
+++ reShutLegacy/variables.cs
@@ -5,13 +5,24 @@ namespace reShutCLI
 {
     internal class Variables
     {
+        // 1.0.3.0 feature toggles
+        public static bool EnableWelcomeUI = false; // Not implemented yet.
+        public static bool EnableSounds = false; // Not implemented yet.
+        public static bool ConfigureReShutCLIAfterWelcomeUI = false; // Not implemented yet.
+        public static bool ResetFunctionEnabled = false; // Not implemented yet.
+        public static bool reShutUIIntegration = false; // Not implemented yet.
+        public static bool RunInBackground = false; // Not implemented yet.
+        public static bool AcceptLicenseInWelcomeUI = false; // Not implemented yet.
+        public static bool AcceptEULAInWelcomeUI = false; // Not implemented yet.
+
+
         // Set true if this is a pre-release
-        public static bool prerelease = true;
+        public static bool prerelease = false;
 
 
         // The version
         public static string version = "1.0.2.0";
-        public static string fullversion = $"v.{version} Beta 2";
+        public static string fullversion = $"v.{version}";
 
         // The motd
         public static string Motd()
