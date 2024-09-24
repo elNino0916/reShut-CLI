a72771e2b94f4654fb6a79a89786a5a37bd91775
Nino <nino.bornemann@gmail.com>
2024-09-24 13:30:52 +0200

    Release 1.0.5.2

--- reShutLegacy/variables.cs
+++ reShutLegacy/variables.cs
@@ -15,7 +15,7 @@ namespace reShutCLI
 
 
         // The version
-        public static string version = "1.0.5.1";
+        public static string version = "1.0.5.2";
 
         // Changes the registry version.
         public static string registryVersion = "5";
