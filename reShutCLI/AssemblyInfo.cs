0c3f9cf4b4b327b59e55a213802c68834f918ef5
Nino <nino.bornemann@gmail.com>
2025-08-16 17:08:28 +0200

    Fix AssemblyInfo

--- reShutCLI/AssemblyInfo.cs
+++ reShutCLI/AssemblyInfo.cs
@@ -1,4 +1,9 @@
+using System.Reflection;
 using System.Runtime.Versioning;
 
 [assembly: SupportedOSPlatform("windows")]
+[assembly: AssemblyTitle("reShut CLI")]
+[assembly: AssemblyProduct("reShut CLI")]
+[assembly: AssemblyDescription("Command-line tool for shutdown, reboot and scheduling")]
+[assembly: AssemblyCompany("elNino0916")]
 
